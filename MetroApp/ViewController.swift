6d7d380fd5aa54160f379a454f6a0c8352cafc77
shdwraze <73839791+shdwraze@users.noreply.github.com>
2023-09-25 18:47:46 +0300

    init project

---
+++ MetroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetroApp
+//
+//  Created by Vitalii Navrotskyi on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
