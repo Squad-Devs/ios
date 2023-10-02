ba856b96409cc8fdd49652d769f80c14a01cb56f
VNavrockij <vitalik.nav@gmail.com>
2023-10-02 18:33:54 +0300

    add: parsing json

---
+++ MetroApp/Model/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  MetroApp
+//
+//  Created by Vitalii Navrotskyi on 02.10.2023.
+//
+
+import Foundation
---
+++ MetroApp/Model/StationManager.swift
@@ -0,0 +1,8 @@
+//
+//  StationManager.swift
+//  MetroApp
+//
+//  Created by Vitalii Navrotskyi on 02.10.2023.
+//
+
+import Foundation
