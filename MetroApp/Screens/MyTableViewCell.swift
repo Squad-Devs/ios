81dba920f4fa25078f382410d29a8c9d76067662
VNavrockij <vitalik.nav@gmail.com>
2023-10-03 19:34:55 +0300

    add: tableView

---
+++ MetroApp/Screens/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  MetroApp
+//
+//  Created by Vitalii Navrotskyi on 03.10.2023.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
