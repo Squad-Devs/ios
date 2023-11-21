4f0a685d56b2a33710b68e16dce8a81236c8faae
VNavrockij <vitalik.nav@gmail.com>
2023-11-21 12:38:26 +0200

    feat: start project without storyboard

--- MetroApp/Screens/MainViewController.swift
+++ MetroApp/Screens/MainViewController.swift
@@ -8,50 +8,11 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    @IBOutlet weak var tableView: UITableView!
-
-    var stations: [OneStation] = [] {
-        didSet {
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-                self.activityIndicator.stopAnimating()
-                print(self.stations[1])
-            }
-        }
-    }
-    let refreshControl = UIRefreshControl()
-    let activityIndicator = UIActivityIndicatorView(style: .large)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        activityIndicator.center = view.center
-        view.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
-
-        tableView.dataSource = self
-        tableView.delegate = self
-
-        let stationManager = StationManager()
-//        stationManager.readLocalJSONFile { [weak self]  result in
-//            self?.stations = result
-//        }
-        stationManager.performRequest { [weak self]  result in
-            self?.stations = result
-            print("Hello")
-        }
-
-        tableView.registerCell(type: MyTableViewCell.self)
-
-        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        tableView.addSubview(refreshControl)
-    }
-
-    @objc func refresh(send: UIRefreshControl) {
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-            self.refreshControl.endRefreshing()
-        }
+        view.backgroundColor = .red
     }
 }
 
